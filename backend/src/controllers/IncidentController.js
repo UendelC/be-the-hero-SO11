0e82894d882f03a40db6676d778dbad0e3d6c7fe
Uendel Couto <uendel.couto@gmail.com>
2020-03-28 10:01:19 -0300

    Adiciona controllers de profile e session

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -2,7 +2,16 @@ const connection = require('../database/connection');
 
 module.exports = {
     async index(request, response) {
-        const incidents = await connection('incidents').select('*');
+        const { page = 1 } = request.query;
+
+        const [count] = await connection('incidents').count();
+
+        const incidents = await connection('incidents')
+            .limit(5)
+            .offset((page - 1) * 5)
+            .select('*');
+
+        response.header('X-Total-Count', count['count(*)']);
 
         return response.json(incidents);
     },
@@ -19,6 +28,20 @@ module.exports = {
         });
 
         return response.json({ id });
+    },
 
+    async delete(request, response) {
+        const { id } = request.params;
+        const ong_id = request.headers.authorization;
+
+        const incident = await connection('incidents').where('id', id).select('ong_id').first();
+
+        if (incident.ong_id !== ong_id) {
+            return response.status(401).json({ error: 'Operation not permitted' });
+        }
+
+        await connection('incidents').where('id', id).delete();
+
+        return response.status(204).send();
     }
 }
